b6047ca1150796466a6bc5bbc868aa48a70d5b6a
nduc4nh <nducanh74@gmail.com>
2026-05-24 22:51:14 +0700

    update resource config

--- ingestion_events/src/ingestion_events/main.py
+++ ingestion_events/src/ingestion_events/main.py
@@ -1,24 +1,49 @@
 import argparse
 from databricks.sdk.runtime import spark
 from ingestion_events import taxis
+import pandas as pd
+import numpy as np
 
+def generate_random_uid_df():
+    uid = int(np.random.randint(0, 1000000) * 2 - np.random.randint(0, 1000))
+    
+    df = pd.DataFrame(
+        {
+            "uid": [uid]
+        }
+    )
+
+    return df
 
 def main():
     # Process command-line arguments
     parser = argparse.ArgumentParser(
         description="Databricks job with catalog and schema parameters",
     )
+
     parser.add_argument("--catalog", required=True)
     parser.add_argument("--schema", required=True)
     args = parser.parse_args()
 
-    # Set the default catalog and schema
-    spark.sql(f"USE CATALOG {args.catalog}")
-    spark.sql(f"USE SCHEMA {args.schema}")
+    dff = spark.createDataFrame(
+        generate_random_uid_df()
+    )
 
-    # Example: just find all taxis from a sample catalog
-    taxis.find_all_taxis().show(5)
+    df = spark.sql(f"""select * from {catalog}.{schema}.ingestion_events""")
+    df = df.union(dff)
+    df.write.format("delta").saveAsTable(f"{catalog}.{schema}.ingestion_events")
+    return
 
+
+
+
+
+
+    
+
+
+
+    
 
 if __name__ == "__main__":
     main()
